920212537e7366635d214272beded45d983475bd
Nataly Jéssica <nataly.jessica.r.souza@gmail.com>
2024-07-27 15:07:01 -0300

    alterando cor do calendario

--- scr/Screens/MainScreen/Calendario.js
+++ scr/Screens/MainScreen/Calendario.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
-import {view, Text, StyleSheet, Button, View} from 'react-native'
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
 
-// localidade do calendário 
+// Configuração de localidade do calendário 
 LocaleConfig.locales['pt-br'] = {
     monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
     monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
@@ -13,44 +13,56 @@ LocaleConfig.locales['pt-br'] = {
 };
 LocaleConfig.defaultLocale = 'pt-br';
 
-const Calendario = () =>{
+const Calendario = () => {
     const [selectedDate, setSelectedDate] = useState('');
     const navigation = useNavigation();
 
-    return(
+    return (
         <View style={styles.container}>
             <Calendar
-            onPress={(day) => setSelectedDate(day.dateString)}
-            markedDates={{
-                [selectedDate]: {selected: true, selectedColor: 'blue', disableTouchEvent: true}
-            }}
-            theme={{
-                todayBackgroundColor: '#00adf5', //cor de hoje
-                todayTextColor: 'black'
-            }}
+                onDayPress={(day) => setSelectedDate(day.dateString)}
+                markedDates={{
+                    [selectedDate]: { selected: true, selectedColor: 'rgb(72,83,250)', disableTouchEvent: true }
+                }}
+                theme={{
+                    todayBackgroundColor: 'rgb(72,83,250)', // Cor de fundo do dia atual
+                    todayTextColor: 'black',
+                    selectedDayBackgroundColor: 'rgb(72,83,250)', // Cor de fundo do dia selecionado
+                    selectedDayTextColor: 'white',
+                    dayTextColor: 'black',
+                    textDisabledColor: 'gray',
+                    arrowColor: 'rgb(72,83,250)', // Cor das setas de navegação
+                    monthTextColor: 'black',
+                    textSectionTitleColor: 'black',
+                   
+                    
+                }}
             />
             <View style={styles.eventContainer}>
                 <Text style={styles.eventText}>Evento para: {selectedDate}</Text>
                 <Button
                     title="Adicionar evento"
-                    onPress={()=> navigation.navigate('Event', {selectedDate})}/>
+                    onPress={() => navigation.navigate('Event', { selectedDate })}
+                />
             </View>
         </View>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      padding: 10,
+        flex: 1,
+
+        padding: 10,
     },
     eventContainer: {
-      marginTop: 20,
-      alignItems: 'center',
+        marginTop: 20,
+        alignItems: 'center',
     },
     eventText: {
-      fontSize: 18,
-      marginBottom: 10,
+        fontSize: 18,
+        marginBottom: 10,
     },
-  });
-  export default Calendario;
+});
+
+export default Calendario;
