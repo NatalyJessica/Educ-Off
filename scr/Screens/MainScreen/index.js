56ae6e8218a47457fb73768bf82b523d5d2ca787
Igor Ximenes <igorximenes27@gmail.com>
2024-09-14 01:33:52 -0300

    adding calendar logic

--- scr/Screens/MainScreen/index.js
+++ scr/Screens/MainScreen/index.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, BackHandler, Alert, FlatList, Modal, TextInput, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, BackHandler, Alert, FlatList, Modal, TextInput, Button, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Calendar } from 'react-native-calendars';
 import { Picker } from '@react-native-picker/picker';
 
-const mockData = {
-  '2024-09-04': [{ name: 'Fazer simulado Fuvest', status: 'A Fazer' }],
-  '2024-09-14': [{ name: 'Prova', status: 'Pronto' }],
-  '2024-09-16': [{ name: 'Simulado', status: 'Fazendo' }],
-};
+const API_URL = 'http://192.168.0.30:8080/v1/tasks'; // Substitua pela URL do seu endpoint
 
 export default function MainScreen() {
   const navigation = useNavigation();
   const [selectedDate, setSelectedDate] = useState('');
-  const [tasks, setTasks] = useState(mockData);
+  const [tasks, setTasks] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [updateModalVisible, setUpdateModalVisible] = useState(false);
   const [newTaskName, setNewTaskName] = useState('');
   const [newTaskStatus, setNewTaskStatus] = useState('A Fazer');
+  const [updateStatus, setUpdateStatus] = useState('A Fazer');
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [updateTaskName, setUpdateTaskName] = useState('');
+  const [updateTaskStatus, setUpdateTaskStatus] = useState('A Fazer');
 
   useEffect(() => {
+    fetchTasks();
+
     const backAction = () => {
       Alert.alert('Sair', 'Deseja realmente sair do aplicativo?', [
         {
@@ -30,49 +33,166 @@ export default function MainScreen() {
         },
         {
           text: 'Sair',
-          onPress: () => BackHandler.exitApp(), // Fecha o aplicativo
+          onPress: () => BackHandler.exitApp(),
         },
       ]);
-      return true; // Indica que o evento do botão "Voltar" foi tratado
+      return true;
     };
     const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
     return () => backHandler.remove();
   }, []);
 
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(API_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        setTasks(data);
+      } else {
+        console.error('Erro ao buscar tarefas:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar tarefas:', error);
+    }
+  };
+
   const handleDayPress = (day) => {
     setSelectedDate(day.dateString);
+    fetchTasks();
     setModalVisible(true);
   };
 
-  const handleAddTask = () => {
-    const newTask = { name: newTaskName, status: newTaskStatus };
-    setTasks((prevTasks) => {
-      const updatedTasks = { ...prevTasks };
-      if (updatedTasks[selectedDate]) {
-        updatedTasks[selectedDate].push(newTask);
+  const handleAddTask = async () => {
+    try {
+      const newTask = { 
+        description: newTaskName, 
+        status: newTaskStatus, 
+        date: selectedDate 
+      };
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newTask),
+      });
+ 
+      if (response.ok) {
+        fetchTasks();
+        setModalVisible(false);
+        setNewTaskName('');
+        setNewTaskStatus('A Fazer');
       } else {
-        updatedTasks[selectedDate] = [newTask];
+        console.error('Erro ao adicionar tarefa:', response.statusText);
       }
-      return updatedTasks;
-    });
-    setModalVisible(false);
-    setNewTaskName('');
-    setNewTaskStatus('A Fazer');
+    } catch (error) {
+      console.error('Erro ao adicionar tarefa:', error);
+    }
+  };
+
+  const handleUpdateTask = async (taskId, newStatus) => {
+    try {
+      const response = await fetch(`${API_URL}/${taskId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ status: newStatus }),
+      });
+ 
+      if (response.ok) {
+        fetchTasks();
+        setUpdateModalVisible(false);
+      } else {
+        console.error('Erro ao atualizar tarefa:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar tarefa:', error);
+    }
+  };
+
+  const handleDeleteTask = async (taskId) => {
+    try {
+      const response = await fetch(`${API_URL}/${taskId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+ 
+      if (response.ok) {
+        fetchTasks();
+      } else {
+        console.error('Erro ao deletar tarefa:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erro ao deletar tarefa:', error);
+    }
+  };
+
+  const handleOpenUpdateModal = (task) => {
+    setSelectedTask(task.task_id);
+    setUpdateTaskName(task.description);
+    setUpdateTaskStatus(task.status);
+    setUpdateModalVisible(true);
   };
 
   const renderTask = ({ item }) => (
-    <View style={styles.todoItem}>
-      <Text style={styles.todoText}>{item.name}</Text>
-      <Text style={styles.todoDate}>{selectedDate}</Text>
+    <View style={[styles.todoItem, { backgroundColor: getStatusColor(item.status) }]}>
+      <View style={styles.iconContainer}>
+        {getStatusIcon(item.status)}
+      </View>
+      <View style={styles.todoContent}>
+        <Text style={styles.todoText}>{item.description}</Text>
+        <Text style={styles.todoDate}>{item.date || selectedDate}</Text>
+      </View>
+      <TouchableOpacity onPress={() => handleOpenUpdateModal(item)} style={styles.iconButton}>
+        <Icon name="pencil" size={20} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => handleDeleteTask(item.task_id)} style={styles.iconButton}>
+        <Icon name="trash" size={20} color="red" />
+      </TouchableOpacity>
     </View>
   );
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case 'A Fazer':
+        return '#f39c12'; 
+      case 'Fazendo':
+        return '#3498db'; 
+      case 'Pronto':
+        return '#2ecc71'; 
+      default:
+        return '#ffffff'; 
+    }
+  };
+
+  const getStatusIcon = (status) => {
+    switch (status) {
+      case 'A Fazer':
+        return <Icon name="hourglass-half" size={20} color="white" />;
+      case 'Fazendo':
+        return <Icon name="spinner" size={20} color="white" />;
+      case 'Pronto':
+        return <Icon name="check-circle" size={20} color="white" />;
+      default:
+        return <Icon name="question-circle" size={20} color="white" />;
+    }
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Página Inicial</Text>
       </View>
-      <View style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
         <Calendar
           onDayPress={handleDayPress}
           markedDates={{
@@ -107,12 +227,12 @@ export default function MainScreen() {
         <View style={styles.todoContainer}>
           <Text style={styles.todoTitle}>Tarefas</Text>
           <FlatList
-            data={tasks[selectedDate] || []}
+            data={tasks}
             renderItem={renderTask}
-            keyExtractor={(item, index) => index.toString()}
+            keyExtractor={(item) => item.task_id.toString()}
           />
         </View>
-      </View>
+      </ScrollView>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Main')}>
           <Icon name="home" size={35} color="black" />
@@ -131,16 +251,14 @@ export default function MainScreen() {
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
+        onRequestClose={() => setModalVisible(false)}
       >
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Adicionar Tarefa</Text>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Criar Tarefa</Text>
             <TextInput
               style={styles.input}
-              placeholder="Nome da Tarefa"
+              placeholder="Descrição da Tarefa"
               value={newTaskName}
               onChangeText={setNewTaskName}
             />
@@ -153,10 +271,40 @@ export default function MainScreen() {
               <Picker.Item label="Fazendo" value="Fazendo" />
               <Picker.Item label="Pronto" value="Pronto" />
             </Picker>
-            <View style={styles.modalButtons}>
-              <Button title="Adicionar" onPress={handleAddTask} color="rgb(72, 83, 227)" />
-              <Button title="Cancelar" onPress={() => setModalVisible(false)} color="rgb(72, 83, 227)" />
-            </View>
+            <Button title="Adicionar Tarefa" onPress={handleAddTask} />
+            <Button title="Fechar" onPress={() => setModalVisible(false)} color="red" />
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={updateModalVisible}
+        onRequestClose={() => setUpdateModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Atualizar Tarefa</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Descrição da Tarefa"
+              value={updateTaskName}
+              onChangeText={setUpdateTaskName}
+            />
+            <Picker
+              selectedValue={updateTaskStatus}
+              style={styles.picker}
+              onValueChange={(itemValue) => setUpdateTaskStatus(itemValue)}
+            >
+              <Picker.Item label="A Fazer" value="A Fazer" />
+              <Picker.Item label="Fazendo" value="Fazendo" />
+              <Picker.Item label="Pronto" value="Pronto" />
+            </Picker>
+            <Button
+              title="Atualizar Tarefa"
+              onPress={() => handleUpdateTask(selectedTask, updateTaskStatus)}
+            />
+            <Button title="Fechar" onPress={() => setUpdateModalVisible(false)} color="red" />
           </View>
         </View>
       </Modal>
@@ -167,128 +315,96 @@ export default function MainScreen() {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    backgroundColor: '#f2f2f2',
   },
   header: {
-    backgroundColor: 'rgb(72, 83, 227)',
-    padding: 16,
-    textAlign: 'center',
+   backgroundColor: 'rgb(72,83,227)',
+    padding: 15,
+    alignItems: 'center',
   },
   headerText: {
-    color: 'white',
-    fontSize: 24,
-    textAlign: 'center',
+    fontSize: 20,
+    color: '#fff',
     fontWeight: 'bold',
   },
-  container: {
+  scrollContainer: {
     flex: 1,
-    justifyContent: 'flex-start',
-    backgroundColor: '#F0F0F0',
-    paddingHorizontal: 20,
   },
   calendar: {
-    marginVertical: 20,
-    borderRadius: 10,
-    elevation: 3,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   todoContainer: {
-    marginVertical: 20,
-    flex: 1,
+    padding: 10,
   },
   todoTitle: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 10,
   },
   todoItem: {
-    padding: 15,
-    backgroundColor: 'white',
-    borderRadius: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
     marginBottom: 10,
-    elevation: 3,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    borderRadius: 5,
+  },
+  iconContainer: {
+    marginRight: 10,
+  },
+  todoContent: {
+    flex: 1,
   },
   todoText: {
     fontSize: 16,
   },
   todoDate: {
     fontSize: 14,
-    color: 'gray',
+    color: '#666',
+  },
+  iconButton: {
+    marginLeft: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    shadowOffset: {
-      width: 0,
-      height: -2,
-    },
-    elevation: 5,
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
+    justifyContent: 'space-around',
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
   },
   button: {
-    backgroundColor: 'white',
+    alignItems: 'center',
   },
-  modalOverlay: {
+  modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  modalView: {
-    backgroundColor: 'white',
-    borderRadius: 10,
+  modalContent: {
+    width: '80%',
+    backgroundColor: '#fff',
     padding: 20,
-    width: '90%',
-    maxWidth: 500,
+    borderRadius: 10,
     shadowColor: '#000',
-    shadowOpacity: 0.3,
+    shadowOpacity: 0.2,
     shadowRadius: 10,
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
+    elevation: 5,
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+  modalTitle: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginBottom: 10,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
-    borderBottomWidth: 1,
-    marginBottom: 20,
-    width: '100%',
-    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
   },
   picker: {
     height: 50,
     width: '100%',
-    marginBottom: 20,
-  },
-  modalButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    marginBottom: 10,
   },
 });
