6c0801a3d4c738d71f198e71e281fb60904f3624
IGOR XIMENES <u22311@lapa31.academico.cotuca.unicamp.br>
2024-11-04 16:50:13 -0300

    adjusting

--- scr/Screens/ContentsScreen/ModulesScreen.js
+++ scr/Screens/ContentsScreen/ModulesScreen.js
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { useFonts, Lustria_400Regular } from '@expo-google-fonts/lustria';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import axios from 'axios';
+import * as FileSystem from 'expo-file-system';
+
+
+import * as Sharing from 'expo-sharing';
+
+
 
 const images = {
   Matemática: require('../../Assets/Matematica.jpg'),
@@ -36,7 +43,47 @@ const ModulesScreen = ({ route, navigation }) => {
   const handleModulePress = (moduleName) => {
     navigation.navigate('LessonScreen', { moduleName });
   };
+
+  const handleDownloadPress = async (moduleName) => {
+    try {
+      console.log(`Iniciando o download do módulo: ${moduleName}`);
   
+      // Faz a requisição com o responseType 'blob'
+      const response = await axios.get(`http://localhost:8080/v1/pdfs/${moduleName}`, {
+        responseType: 'blob',
+      });
+      console.log(`Resposta recebida:`, response);
+  
+      const blob = new Blob([response.data]);
+      const reader = new FileReader();
+  
+      reader.onloadend = async () => {
+        const base64data = reader.result;
+        const fileUri = FileSystem.documentDirectory + `${moduleName}.pdf`;
+        console.log(`Caminho do arquivo: ${fileUri}`);
+  
+        try {
+          // Salva o arquivo no sistema de arquivos
+          await FileSystem.writeAsStringAsync(fileUri, base64data.split(',')[1], {
+            encoding: FileSystem.EncodingType.Base64,
+          });
+          
+          alert(`Módulo ${moduleName} baixado com sucesso!`);
+  
+          // Compartilha o arquivo PDF
+          await Sharing.shareAsync(fileUri);
+        } catch (writeError) {
+          console.error('Erro ao salvar o arquivo:', writeError);
+          alert(`Houve um problema ao salvar o arquivo: ${writeError.message}`);
+        }
+      };
+  
+      reader.readAsDataURL(blob); // Converte o blob para base64
+    } catch (error) {
+      console.error('Erro ao baixar o módulo:', error);
+      alert(`Houve um problema ao baixar o módulo: ${error.message}`);
+    }
+  };
   const renderModule = ({ item }) => (
     <TouchableOpacity style={styles.moduleContainer} onPress={() => handleModulePress(item.module_name)}>
       <Image source={getImage(subjectName)} style={styles.moduleImage} />
@@ -45,6 +92,9 @@ const ModulesScreen = ({ route, navigation }) => {
         {item.done && (
           <Icon name="check-circle" size={24} color="green" style={styles.doneIcon} />
         )}
+        <TouchableOpacity onPress={() => handleDownloadPress(item.module_name)} style={styles.downloadButton}>
+          <Icon name="file-download" size={24} color="#333" />
+        </TouchableOpacity>
       </View>
     </TouchableOpacity>
   );
@@ -82,7 +132,7 @@ const ModulesScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 0, // Adiciona espaço suficiente para o cabeçalho
+    paddingTop: 0,
   },
   header: {
     backgroundColor: 'rgb(72,83,227)',
@@ -132,9 +182,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
     marginRight: 10,
+    flex: 1,
   },
   doneIcon: {
     marginLeft: 5,
+  },
+  downloadButton: {
+    marginLeft: 10,
+    padding: 8,
   },
   flatListContent: {
     paddingBottom: 20,
